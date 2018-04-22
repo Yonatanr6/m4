21ebe61ffcb97fda9a1f628ae2db70b7767c78d7
Yonatanr6 <33617974+Yonatanr6@users.noreply.github.com>
2018-04-22 13:03:29 +0300

    Add files via upload

---
+++ CircularInt.cpp
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include "CircularInt.hpp"
+#include <iostream>
+using namespace std;
+
+  ostream& CircularInt::operator<<(CircularInt hour){
+      return cout << hour.curr;
+  }
+   CircularInt& CircularInt::operator+(int a){
+//       if(this->curr+a<this->max_range){
+//           this->curr=this->curr+a;
+//            return this->curr;
+//       }
+//       if(this->curr+a>this->max_range)
+    }
+    
+    CircularInt& CircularInt::operator-(int a){
+        
+    }
+    CircularInt& CircularInt::operator*(int a){
+        
+    }
+    CircularInt& CircularInt::operator/(int a){
+        
+    }
+    
+    CircularInt& CircularInt::operator+=(int a){
+        
+    }
+    CircularInt& CircularInt::operator-=(int a){
+        
+    }
+    CircularInt& CircularInt::operator/=(int a){
+        
+    }
+    CircularInt& CircularInt::operator*=(int a){
+        
+    }
+    
+    CircularInt& CircularInt::operator++(){
+        
+    }
+    const CircularInt& CircularInt::operator++(int a){
+        
+    }
+    
+    CircularInt& CircularInt::operator--(){
+        
+    }
+    CircularInt& CircularInt::operator--(int a){
+        
+    }
---
+++ CircularInt.hpp
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   CircularInt.hpp
+ * Author: Yoni
+ *
+ * Created on April 21, 2018, 9:01 PM
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class CircularInt{
+    
+ public:   
+    int min_range, max_range, dis_max,curr;
+    
+
+    ostream& operator<<(CircularInt hour);//return cout << obj.
+    CircularInt& operator+(int a);
+    CircularInt& operator-(int a);
+    CircularInt& operator*(int a);
+    CircularInt& operator/(int a);
+    
+    CircularInt& operator+=(int a);
+    CircularInt& operator-=(int a);
+    CircularInt& operator/=(int a);
+    CircularInt& operator*=(int a);
+    
+    CircularInt& operator++();
+    const CircularInt& operator++(int a);
+    
+    CircularInt& operator--();
+    CircularInt& operator--(int a);
+    
+    CircularInt(int a,int b){
+        if (a>b){
+            max_range=a;
+            min_range=b;
+            curr=min_range;
+        }
+        if(b>a){
+            max_range=b;
+            min_range=a;
+            curr=min_range;
+        }
+        else
+            cout << "There is no range!" << endl;
+    }
+    
+};
