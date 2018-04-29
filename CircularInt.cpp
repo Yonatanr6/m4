7c2c5ebd2af8c03be56dde8ceaa54123aa10c052
Yonatanr6 <33617974+Yonatanr6@users.noreply.github.com>
2018-04-29 16:29:37 +0300

    Add files via upload

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -14,8 +14,7 @@ using namespace std;
       out << "";
   }
    CircularInt& CircularInt::operator+(int a){
-        this->curr= ((this->curr-this->min_range)+(a%(this->max_range-this->min_range+1)))%(this->max_range-this->min_range+1)+this->min_range;
-           return *this; 
+       
     }
     
 //    CircularInt& CircularInt::operator-(int a){
